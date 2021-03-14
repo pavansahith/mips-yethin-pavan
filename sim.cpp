d23467cea1179b7c13644a0fc4a44f09ca1e4238
pavansahith <cs19b017@iittp.ac.in>
2021-03-14 22:24:13 +0530

    init

--- sim.cpp
+++ sim.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
-nsNDSJAWDNKADNKAD
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct labels{
+    string label;
+    int32_t line_number_of_label;
+};
+
+struct memory_elements{
+    string name;
+    int start_address;
+};
